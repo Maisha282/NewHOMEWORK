c7c2e11ea992dd8eb1c3a4cc96f579be9bf7637b
Maisha282 <99607747+Maisha282@users.noreply.github.com>
2022-02-22 12:26:00 +0600

    Add files via upload

---
+++ StInformation.java
@@ -0,0 +1,22 @@
+
+public class StInformation {
+
+	public static void main(String[] args) {
+		// Creating an object of Student Class
+		Student s1 = new Student();
+		s1.name = "Maisha Rahman";
+		s1.id = 2122153642;
+		s1.cgpa = 3.7;
+		s1.StudentInfo();	
+		Student s2 = new Student();
+		s2.name = "X Khan";
+		s2.id = 2122255642;
+		s2.cgpa = 3.5;
+		s2.StudentInfo();
+
+		System.out.println(s1.counter);
+		System.out.println(s2.counter);
+		
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,25 @@
+
+public class Student {
+	// three data fields mentioned in the question
+        String name;
+        int id;
+        double cgpa;
+         int counter = 0;
+      Student(){// No-Argument Constructor or Default Constructor
+	   counter++;
+ }
+   // Argument Constructor or Parameter wised Constructor
+    Student(String name, int id, double cgpa){
+	    this.name = name;
+	    this.id = id;
+	    this.cgpa = cgpa;
+	    counter++;
+ }
+   // methods
+        void StudentInfo() {
+	   System.out.println(" Student Name: "+name);
+	   System.out.println(" Student ID: "+id);
+	   System.out.println(" Student CGPA: "+cgpa);
+	   System.out.println();
+   }
+}
